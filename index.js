6ed8ac72f255e8c10f1eb4b2e10f2927366f5148
arjun-raj1523 <arjraj1523@gmail.com>
2016-05-13 18:28:43 -0500

    test

--- index.js
+++ index.js
@@ -19,4 +19,29 @@ app.get('/users', function(req, res) {
     }
   });
 });
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 app.listen(3000)
+module.exports = app;
