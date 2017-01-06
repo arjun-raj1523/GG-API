65db76bf508291cc955d6a0eb3cacf775757ff6c
arjun-raj1523 <arjraj1523@gmail.com>
2017-01-06 07:50:43 +0400

    Structure Change with new idea and APi simplification for MVP

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var path = require('path');
 var bodyParser = require("body-parser");
+
 app.set('port', (process.env.PORT || 5000));
 
 //For POST method
@@ -9,18 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Modular Declaration
-var get_user_creds = require('./javascripts/get_user_creds');
-var put_user_creds = require('./javascripts/put_user_creds');
+
 var get_user_goals = require('./javascripts/get_user_goals');
 var put_user_goals = require('./javascripts/put_user_goals');
-var get_user_goals_content = require('./javascripts/get_user_goals_content');
-var put_user_goals_content = require('./javascripts/put_user_goals_content');
-app.use('/get_user_creds',get_user_creds);
-app.use('/put_user_creds',put_user_creds);
+
+
 app.use('/get_user_goals',get_user_goals);
 app.use('/put_user_goals',put_user_goals);
-app.use('/get_user_goals_content',get_user_goals_content);
-app.use('/put_user_goals_content',put_user_goals_content);
+
 
 
 //For avoidong Heroku $PORT error
--- javascripts/get_user_goals.js
+++ javascripts/get_user_goals.js
@@ -1,51 +1,37 @@
 var express = require('express');
+
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("./goalgettr-74966-firebase-adminsdk-u3g16-1f2fc8432a.json"),
+  databaseURL: "https://goalgettr-74966.firebaseio.com"
+});
 
 /*
   To Get JSON for specific email id
 */
-router.get('/:id', function(req, res){
+router.get('/:id', function(request, response){
   //Create Variable for id
-  var user_id = req.params.id;
-    //Connect to DB
-  MongoClient.connect("mongodb://ggadmin:khiladi720@ds045521.mlab.com:45521/gg_user", function(err, db) {
-    if(!err) {
-      //Collection Variable to get Data
-      var collection = db.collection('gg_user_goals');
-      //MongoDB select statement with hardcoded value
-      collection.find({userid:user_id}).toArray(function(err, items) {
-        res.json(items);
-      });
-    }
-    else {
-      return console.log(err);
-    }
+  var db = admin.database();
+  var ref = db.ref("restricted_access/secret_document");
+  ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+    response.send(snapshot.val());
   });
+
 });
 
 /*
   To get JSON for all user ID
 */
 
-router.get('/', function(req, res){
+router.get('/', function(request, response){
   //Connect to DB
-  var emailID = req.params.emailID;
+  console.log("Works");
+  response.send("Works");
 
-  MongoClient.connect("mongodb://ggadmin:khiladi720@ds045521.mlab.com:45521/gg_user", function(err, db) {
-    if(!err) {
-      //Collection Variable to get Data
-      var collection = db.collection('gg_user_goals');
-      //MongoDB select statement with hardcoded value
-      collection.find({}).toArray(function(err, items) {
-        // To get User ID. console.log(items[0]._id);
-        res.json(items);
-      });
-    }
-    else {
-      return console.log(err);
-    }
-  });
 });
 
 module.exports = router;
