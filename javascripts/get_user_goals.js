245acafa287648d55e8a0ed3a9d5e0877a5cebcf
arjun-raj1523 <arjraj1523@gmail.com>
2017-01-08 13:19:03 +0400

    API setup for get and out methods-Get data for particular user

--- javascripts/get_user_goals.js
+++ javascripts/get_user_goals.js
@@ -5,13 +5,29 @@ var router = express.Router();
 /*
   To Get JSON for specific email id
 */
-router.get('/:id', function(request, response){
+router.get('/:userid', function(request, response){
   //Create Variable for id
   var db = admin.database();
-  var ref = db.ref("restricted_access/secret_document");
+  var ref = db.ref("server/users");
+  var userid = request.params.userid;
+  var flagFound = false;
+  var dataObject = null;
   ref.once("value", function(snapshot) {
-    console.log(snapshot.val());
-    response.send(snapshot.val());
+    //console.log(snapshot.val());
+    
+    snapshot.forEach(function(data){
+      //response.send(data.val());
+      if(data.val().userid == userid){
+        flagFound = true;
+        dataObject = data.val();
+      }
+    });
+    if(flagFound)
+
+      response.send({status:"true",data:dataObject});
+    else
+      response.send({status:"false"})
+
   });
 
 });
@@ -23,8 +39,17 @@ router.get('/:id', function(request, response){
 router.get('/', function(request, response){
   //Connect to DB
   console.log("Works");
-  response.send("Works");
 
+  var db = admin.database();
+  var ref = db.ref("server/saving-data/fireblog/users/");
+  ref.once("value", function(snapshot) {
+  //Go through each key
+  snapshot.forEach(function(data) {
+    console.log("The " + data.key + " dinosaur's score is " + data.val().author);
+  });
+//All Data from users
+    response.send(snapshot.val());
+  });
 });
 
 module.exports = router;
--- javascripts/put_user_goals.js
+++ javascripts/put_user_goals.js
@@ -8,28 +8,35 @@ router.get('/',function(request,response){
 
 // Get a database reference to our blog
 var db = admin.database();
-var ref = db.ref("server/saving-data/fireblog");
+var ref = db.ref("server/");
 var usersRef = ref.child("users");
-  usersRef.child("alanisawesome").set({
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  });
-  usersRef.child("gracehop").set({
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  });
+
+  // usersRef.child("alanisawesome").set({
+  //   date_of_birth: "June 23, 1912",
+  //   full_name: "Alan Turing"
+  // });
+  // usersRef.child("gracehop").set({
+  //   date_of_birth: "December 9, 1906",
+  //   full_name: "Grace Hopper"
+  // });
 
 });
 
-router.get('/add',function(request,response){
+//Call on Account Create Success
+router.get('/add/:userid/:username',function(request,response){
 
 // Get a database reference to our blog
 var db = admin.database();
-var ref = db.ref("server/saving-data/fireblog");
+var ref = db.ref("server");
 var usersRef = ref.child("users");
-  usersRef.child("alanisawesweome").set({
-    date_of_birth: "June 23, 1912",
-    full_name: "Alwe Turing"
+
+var userid = request.params.userid;
+var username = request.params.username;
+
+  usersRef.push().set({
+    name: username,
+    userid: userid,
+    goals:""
   },function(error){
     if(error){
       console.log("Error");
@@ -52,25 +59,41 @@ router.get('/update',function(request,response){
     
   });
 
-
 });
 
-router.get('/addlist/:userid',function(request,response){
+
+//Add Goal for user.
+router.get('/addgoal/:userid/:goalName/:goalType',function(request,response){
+
   var db = admin.database();
-  var ref = db.ref("server/saving-data/fireblog");
-
-  var postsRef = ref.child("users/"+request.params.userid+"/goals");
-
-
-  // we can also chain the two calls together
-  postsRef.push().set({
-    author: "alanisawesome",
-    title: "The Turing Machine"
-  },function(error){
-    if(!error){
-      response.send("Added");
-    }
+  var ref = db.ref("server");
+  var userid = request.params.userid;
+  var userKey = "";
+  var goalName = request.params.goalName;
+  var goalType = request.params.goalType;
+  var ref = db.ref("server/users");
+  ref.once("value", function(snapshot) {
+  //Go through each key
+    snapshot.forEach(function(data) {
+      if(data.val().userid == userid){
+        userKey=data.key;
+        // // we can also chain the two calls together
+        var goalRef = ref.child(userKey+"/goals");
+        goalRef.push().set({
+          goalName:goalName,
+          goalType:goalType
+        },function(error){
+          if(!error){
+            response.send("Added");
+          }
+        });
+      }
+    });
   });
+
+
+
+
 
 });
 
